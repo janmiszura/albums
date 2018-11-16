f31c6b64dd3c2a9eb12a96e75cdc960ef83d782a
Jan Miszura <jan@ilion.com.br>
2018-11-16 13:40:16 -0200

    instalando axios para requisições http

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # albums
 Curso de React-Native
+
+
+#troubleshooting
+- quando der erro de "ENOSPC" ao executar "react-native start"
+$ echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import axios from 'axios';
 
 export default class AlbumList extends Component {
+  
+  state = { albums: [] };
+
+  componentDidMount() {
+    console.log('componentDidMount on AlbumList');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then( response => console.log(response) );
+  }
+
+  renderAlbums() {
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>AlbumList</Text>
+        <Text style={styles.text}>AlbumList!!!!</Text>
       </View>
     );
   }
